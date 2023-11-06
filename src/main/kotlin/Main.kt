bd1befdd537d3af6d936c2650460b0009aa17b3b
andresam <andres@aamsis.co>
2023-11-06 08:32:28 -0500

    add algorithm

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+/*
+Length of the longest substring without repeating characters
+Given a string str, find the length of the longest substring without repeating characters.
+
+Example:
+For “ABDEFGABEF”, the longest substring are “BDEFGA” and “DEFGAB”, with length 6.
+
+For “BBBB” the longest substring is “B”, with length 1.
+
+For “GEEKSFORGEEKS”, there are two longest substrings shown in the below diagrams, with length 7
+
+*/
+
+
+fun main(args: Array<String>) {
+    val testText = "ABDEFGABEF"
+    val splitString = testText.split("")
+    var longestString = ""
+    for (i in 0..<splitString.size) {
+        var localLongestString = splitString[i]
+        for (j in i + 1..<splitString.size) {
+            if (localLongestString.contains(splitString[j])) {
+                if (localLongestString.length > longestString.length) {
+                    longestString = localLongestString
+                }
+                break
+            } else {
+                localLongestString = "$localLongestString${splitString[j]}"
+            }
+        }
+    }
+
+    print(longestString)
+}
